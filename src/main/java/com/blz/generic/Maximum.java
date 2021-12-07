376bfd21660ca5aeb534de11c64b6cee8730b41e
Manishakahar <manisha.kahar1998@gmail.com>
2021-12-08 03:03:37 +0530

    Added: check Float Maximum Value.

--- src/main/java/com/blz/generic/Maximum.java
+++ src/main/java/com/blz/generic/Maximum.java
@@ -1,11 +1,13 @@
 package com.blz.generic;
-
+// Main Class
 public class Maximum {
+    //Main Method
     public static void main(String[] args) {
-        System.out.println("Welcome Generics Problem");
+        System.out.println("Welcome Generics Problem"); // Print Welcome Message
     }
-
+// Passed the Float Value
     public static Float getMax(Float a, Float b, Float c) {
+        // Compare the Float Value
         if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
             return a;
         } else if (b.compareTo(a) > 0 && b.compareTo(c) > 0) {
--- src/test/java/com/blz/generic/MaximumTest.java
+++ src/test/java/com/blz/generic/MaximumTest.java
@@ -5,26 +5,29 @@ import org.junit.Test;
 
 public class MaximumTest {
 
+    //Test Case for the Float where max_Value at First Position
     @Test
     public void given3FloatValue_WhenGetMaximumNO_ShouldReturnCorrectMaxValue() {
         Maximum maximum = new Maximum();
-        Float actualResult = Maximum . getMax(5.6f,5.5f,15.5f);
-        Float expectedResult = 15.5f;
+        Float actualResult = Maximum . getMax(15.6f,10.5f,11.5f);
+        Float expectedResult = 15.6f;
         Assert.assertEquals(expectedResult,actualResult);
     }
 
+     //  Test Case for the Float where max_Value at Second Position
     @Test
     public void given3FloatValue_Case2_WhenGetMaximumNO_ShouldReturnCorrectMaxValue() {
         Maximum maximum = new Maximum();
-        Float actualResult = Maximum . getMax(5.1f,12.5f,5.6f);
+        Float actualResult = Maximum . getMax(11.1f,12.5f,10.6f);
         Float expectedResult = 12.5f;
         Assert.assertEquals(expectedResult,actualResult);
     }
+    //  Test Case for the Float where max_Value at Third Position
     @Test
     public void given3FloatValue_Case3_WhenGetMaximumNO_ShouldReturnCorrectMaxValue() {
         Maximum maximum = new Maximum();
-        Float actualResult = Maximum . getMax(10.5f,5.6f,5.5f);
-       Float expectedResult = 10.5f;
+        Float actualResult = Maximum . getMax(10.5f,15.6f,16.5f);
+       Float expectedResult = 16.5f;
         Assert.assertEquals(expectedResult,actualResult);
     }
 
