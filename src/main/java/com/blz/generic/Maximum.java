b6355605704fe3896dea92e74e4cbf8a6c0facf1
Manishakahar <manisha.kahar1998@gmail.com>
2021-12-08 02:27:01 +0530

    Added: Checking  Maximum Integer Value Useing TestCase.

--- src/main/java/com/blz/generic/Maximum.java
+++ src/main/java/com/blz/generic/Maximum.java
@@ -4,5 +4,15 @@ public class Maximum {
     public static void main(String[] args) {
         System.out.println("Welcome Generics Problem");
     }
+    static Integer getMax(Integer a, Integer b, Integer c) {
+        if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
+            return a;
+        }
+        else if (b.compareTo(a) > 0 && b.compareTo(c) >0){
+            return b;
+        }else {
+            return c;
+        }
 
+    }
 }
