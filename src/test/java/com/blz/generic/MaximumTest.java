4e38ba682fd854a7b698c23b5584e990345f570b
Manishakahar <manisha.kahar1998@gmail.com>
2021-12-08 02:43:07 +0530

    Added: check Float Maximum Value.

--- src/test/java/com/blz/generic/MaximumTest.java
+++ src/test/java/com/blz/generic/MaximumTest.java
@@ -6,25 +6,25 @@ import org.junit.Test;
 public class MaximumTest {
 
     @Test
-    public void given3IntegerValue_WhenGetMaximumNO_ShouldReturnCorrectMaxValue() {
+    public void given3FloatValue_WhenGetMaximumNO_ShouldReturnCorrectMaxValue() {
         Maximum maximum = new Maximum();
-        int actualResult = Maximum . getMax(10,5,16);
-        int expectedResult = 16;
+        Float actualResult = Maximum . getMax(5.6f,5.5f,15.5f);
+        Float expectedResult = 15.5f;
         Assert.assertEquals(expectedResult,actualResult);
     }
 
     @Test
-    public void given3IntegerValue_Case2_WhenGetMaximumNO_ShouldReturnCorrectMaxValue() {
+    public void given3FloatValue_Case2_WhenGetMaximumNO_ShouldReturnCorrectMaxValue() {
         Maximum maximum = new Maximum();
-        int actualResult = Maximum . getMax(10,15,11);
-        int expectedResult = 15;
+        Float actualResult = Maximum . getMax(5.1f,12.5f,5.6f);
+        Float expectedResult = 12.5f;
         Assert.assertEquals(expectedResult,actualResult);
     }
     @Test
-    public void given3IntegerValue_Case3_WhenGetMaximumNO_ShouldReturnCorrectMaxValue() {
+    public void given3FloatValue_Case3_WhenGetMaximumNO_ShouldReturnCorrectMaxValue() {
         Maximum maximum = new Maximum();
-        int actualResult = Maximum . getMax(12,10,11);
-        int expectedResult = 12;
+        Float actualResult = Maximum . getMax(10.5f,5.6f,5.5f);
+       Float expectedResult = 10.5f;
         Assert.assertEquals(expectedResult,actualResult);
     }
 
